860bd1f93c78ad08d8783fee98fb552bcad87f87
rnhutsko <rnhutsko@gmail.com>
2016-06-09 13:53:40 -0400

    added basic outline and started to fill out dependencies

--- lib/cars_for_sale.rb
+++ lib/cars_for_sale.rb
@@ -1,4 +1,12 @@
-require "cars_for_sale/version"
+require 'open-uri'
+require 'nokgiri'
+require 'pry'
+
+
+
+require_relative "cars_for_sale/version"
+require_relative './cars_for_sale/cli'
+
 
 module CarsForSale
   # Your code goes here...
---
+++ lib/cars_for_sale/cli.rb
@@ -0,0 +1,11 @@
+class Cars-for-Sale::CLI
+
+def call
+  puts "Welcome to Cars.com cars for sale..."
+  puts "Which group of cars do wish to see? "
+  puts "1-25 or 26-50 or exit?"
+  puts "Enter Group Choice:"
+
+
+
+end
